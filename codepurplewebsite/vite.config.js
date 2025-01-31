6a2fba6c6f1c5845481987d0c617012b426907c8
TejasDoesStuff <tejaspanja@gmail.com>
2025-01-30 16:56:06 -0800

    pushin p

--- codepurplewebsite/vite.config.js
+++ codepurplewebsite/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   assetsInclude: ['**/*.JPG'],
-  base: '/codepurplewebsite/',
+  base: '/codepurpleofficial/',
   build: {
     outDir: "dist",
   },
